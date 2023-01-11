006b227d5d897b197593afaacdeda167f9cdcf21
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-01-11 05:34:59 +0000

    Added player registration feature with validation for email address

--- validation2.py
+++ validation2.py
@@ -160,44 +160,4 @@ def register_player(username, email):
         print(f"Invalid email address: {e}")
         return False
 
-    # Check if the login details are already in use
-    if check_login(username, email):
-        print("Username and email are already in use")
-        return False
-
-    # Authenticate with Google Sheets API
-    creds = Credentials.from_service_account_file(CREDS_FILE, scopes=SCOPES)
-    client = gspread.authorize(creds)
-
-    # Open the sheet
-    sheet = client.open(SHEET_NAME).sheet1
-
-    # Add the new player to the sheet
-    sheet.append_row([username, email])
-    print("Registration successful")
-
-# Prompt player1 to enter login details
-player1_username = input("Player 1, please enter your chosen username: ")
-player1_email = input("Player 1, please enter your email address: ")
-
-# Prompt player2 to enter login details
-player2_username = input("Player 2, please enter your chosen username: ")
-player2_email = input("Player 2, please enter your email address: ")
-
-# Register player1
-if register_player(player1_username, player1_email):
-    print("Player 1 registered successfully")
-else:
-    print("Player 1 registration failed")
-    print("try again")
-    register_player(username=None, email=None)
-
-# Register player2
-if register_player(player2_username, player2_email):
-    print("Player 2 registered successfully")
-    print("Welcome to Tic Tac Toe!")
-    start_tic_tac_toe()
-else:
-    print("Player 2 registration failed")
-    print("try again")
-    register_player(username=None, email=None)
+ 
