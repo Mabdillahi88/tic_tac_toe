abd535d6c94473aacbf110ae996eef4929aef7da
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-01-11 05:01:49 +0000

    Added player registration feature with email validation and existing login check

--- validation2.py
+++ validation2.py
@@ -4,6 +4,8 @@ from google.oauth2.service_account import Credentials
 import time
 from email_validator import validate_email, EmailNotValidError
 
+
+
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -13,44 +15,26 @@ SCOPES = [
 CREDS_FILE = 'creds.json'
 SHEET_NAME = 'Tic_tac_toe'
 
-def check_login(username, email):
+
+def register_player(username = None, email = None):
+    """
+    Register a new player using the provided username and email.
+    
+    Parameters:
+        username (str): The desired username.
+        email (str): The email address to associate with the username.
+    """
     # Validate the email address
-    try:
-        v = validate_email(email)
-        email = v["email"]  # Replace the email with the normalized form
-    except EmailNotValidError as e:
-        print(f"Invalid email address: {e}")
-        return False
+    if username and email: 
+        try:
+            v = validate_email(email)
+            email = v["email"]  # Replace the email with the normalized form
+        except EmailNotValidError as e:
+            print(f"Invalid email address: {e}")
+            return False
 
-    # Authenticate with Google Sheets API
-    creds = Credentials.from_service_account_file(CREDS_FILE, scopes=SCOPES)
-    client = gspread.authorize(creds)
+        # Check if the login details are already in use
+        if check_login(username, email):
+            print("Username and email are already in use")
+            return False
 
-    # Open the sheet
-    sheet = client.open(SHEET_NAME).sheet1
-
-    # Search for the username and email in the sheet
-    cell = sheet.find(username)
-    if cell is None:
-        return False
-    row = cell.row
-    if sheet.cell(row, 2).value != email:
-        return False
-
-    return True
-
-player1_username = input("Player 1, please enter your username: ")
-player1_email = input("Player 1, please enter your email address: ")
-
-player2_username = input("Player 2, please enter your username: ")
-player2_email = input("Player 2, please enter your email address: ")
-
-if check_login(player1_username, player1_email):
-    print("Player 1 login successful")
-else:
-    print("Player 1 login failed")
-
-if check_login(player2_username, player2_email):
-    print("Player 2 login successful")
-else:
-    print("Player 2 login failed")
