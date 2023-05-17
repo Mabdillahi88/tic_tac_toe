14d9837f008667a5c9011f955a94f41a055c1477
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-05-17 15:23:24 +0000

    Noughts and Crosses game class and implement move functionality

--- run.py
+++ run.py
@@ -30,4 +30,20 @@ def separate_line():
     """
     print("\n" + "-"*30 + "\n")
 
+BOARD_SIZE = 3
 
+class NoughtsAndCrossesGame:
+    def __init__(self):
+        self.board = [[" " for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+        self.current_player = random.choice(["X", "O"])
+
+    def switch_player(self):
+        self.current_player = "O" if self.current_player == "X" else "X"
+
+    def make_move(self, row, column):
+        if 0 <= row < BOARD_SIZE and 0 <= column < BOARD_SIZE and self.board[row][column] == " ":
+            self.board[row][column] = self.current_player
+            return True
+        return False
+
+    
